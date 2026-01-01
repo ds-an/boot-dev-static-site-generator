b16d62eb1ac56b9efc3b9cb3cbd37b76f4afe663
Andrew Malyukov <ds-an@tuta.io>
2026-01-01 22:09:54 +0200

    feat: markdown_to_html_block function, first iteration

--- src/block_handling.py
+++ src/block_handling.py
@@ -1,5 +1,8 @@
 from enum import Enum
 import re
+from htmlnode import LeafNode, ParentNode
+from textnode import text_node_to_html_node
+from node_operations import text_to_textnodes
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -12,7 +15,6 @@ class BlockType(Enum):
 def block_to_block_type(markdown_block):
     split_markdown_block = markdown_block.split("\n")
     split_markdown_block = list(filter(lambda x: x != "", split_markdown_block))
-    print(split_markdown_block)
     if re.match(r"^#{1,6} .+", split_markdown_block[0]):
         return BlockType.HEADING
     if markdown_block.startswith("```")\
@@ -54,3 +56,43 @@ def markdown_to_blocks(markdown):
         line = line.strip()
         blocks.append(line)
     return blocks
+
+def markdown_to_html_node(markdown):
+    markdown_blocks = markdown_to_blocks(markdown)
+    print(markdown_blocks)
+    markdown_block_types = list(map(block_to_block_type, markdown_blocks))
+    markdown_blocks_and_types = list(zip(markdown_blocks, markdown_block_types))
+    html_nodes = []
+    for block, block_type in markdown_blocks_and_types:
+        match block_type:
+            case block_type.QUOTE:
+                html_nodes.append(ParentNode("blockquote", list(map(text_node_to_html_node, text_to_textnodes(block)))))
+            case block_type.PARAGRAPH:
+                html_nodes.append(ParentNode("p", list(map(text_node_to_html_node, text_to_textnodes(block)))))
+            case block_type.HEADING:
+                heading_level = 0 
+                for i in range(len(block)):
+                    if block[i] == "#":
+                        heading_level = heading_level + 1
+                    else:
+                        break
+                html_nodes.append(ParentNode(f"h{heading_level}", list(map(text_node_to_html_node, text_to_textnodes(block)))))
+            case block_type.CODE:
+                pre_node = ParentNode("pre", ParentNode("code", text_node_to_html_node(block)))
+                html_nodes.append(pre_node)
+            case block_type.UNORDERED_LIST:
+                split_list = block.split("\n")
+                split_list = list(filter(lambda x: x != "", split_list))
+                print(split_list)
+                list_items = []
+                for list_item in split_list:
+                    list_items.append(ParentNode("li", list(map(text_node_to_html_node, text_to_textnodes(list_item)))))
+                html_nodes.append(ParentNode("ul", list_items))
+            case block_type.ORDERED_LIST:
+                split_list = block.split("\n")
+                split_list = list(filter(lambda x: x != "", split_list))
+                list_items = []
+                for list_item in split_list:
+                    list_items.append(ParentNode("li", list(map(text_node_to_html_node, text_to_textnodes(list_item)))))
+                html_nodes.append(ParentNode("ol", list_items))
+    return ParentNode("div", html_nodes)
--- src/test_block_handling.py
+++ src/test_block_handling.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_handling import BlockType, markdown_to_blocks, block_to_block_type
+from block_handling import BlockType, markdown_to_blocks, block_to_block_type, markdown_to_html_node
 
 class TestBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -99,3 +99,41 @@ This is a paragraph of text. It has some **bold** and _italic_ words inside of i
             block_type,
             BlockType.PARAGRAPH
         )
+    def test_markdown_to_html_node(self):
+        md = "1. This is an ordered list\n2. That continues here\n3. And ends here\n\n- This is an unordered list\n- That continues here\n- And ends here"
+        block_type = markdown_to_html_node(md)
+        self.assertEqual(
+            block_type,
+            BlockType.PARAGRAPH
+        )
+    def test_markdown_to_html_node_paragraphs(self):
+        md = """
+    This is **bolded** paragraph
+    text in a p
+    tag here
+
+    This is another paragraph with _italic_ text and `code` here
+
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    # def test_markdown_to_html_node_codeblock(self):
+    #     md = """
+    # ```
+    # This is text that _should_ remain
+    # the **same** even with inline stuff
+    # ```
+    # """
+    #
+    #     node = markdown_to_html_node(md)
+    #     html = node.to_html()
+    #     self.assertEqual(
+    #         html,
+    #         "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+    #     )
